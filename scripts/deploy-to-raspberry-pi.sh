b7c9ac3b70e1d3afacdd9419c0add95e85eb673f
Come Turrata <come@getluko.com>
2020-11-21 16:01:07 +0100

    Add script to deploy this repository to a raspberry pi

---
+++ scripts/deploy-to-raspberry-pi.sh
@@ -0,0 +1,17 @@
+
+backups_directory="escape-server-backups"
+
+echo "Make a backup of current 'escape-server' directory"
+sshpass -p 'raspberry' ssh pi@raspberrypi.local "
+mkdir -p $backups_directory;
+tar -czf $backups_directory/\$(date +%F_%H-%M-%S).tar.gz escape-server;
+"
+
+echo "Delete current 'escape-server' directory"
+sshpass -p 'raspberry' ssh pi@raspberrypi.local "
+sudo rm -rf escape-server;"
+
+current_git_directory=`git rev-parse --show-toplevel`
+
+echo "Copy Current git respository to rasperry pi"
+sshpass -p 'raspberry' scp -r $current_git_directory pi@raspberrypi.local:/home/pi
