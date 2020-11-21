0f3d2237ea7a1ffcb3a9bef788299075fd86dd76
Come Turrata <come@getluko.com>
2020-11-21 16:12:53 +0100

    Improve script

--- scripts/deploy-to-raspberry-pi.sh
+++ scripts/deploy-to-raspberry-pi.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-backups_directory="escape-server-backups"
+BACKUPS_DIRECTORY="escape-server-backups"
+RASPBERRYPI_PASSWORD="raspberry"
 
 echo "1/3. Backing up current 'escape-server' directory on rasperry pi..."
-sshpass -p 'raspberry' ssh pi@raspberrypi.local "
-mkdir -p $backups_directory;
-tar -czf $backups_directory/\$(date +%F_%Hh%Mm%Ss).tar.gz escape-server;
+sshpass -p $RASPBERRYPI_PASSWORD ssh pi@raspberrypi.local "
+mkdir -p $BACKUPS_DIRECTORY;
+tar -czf $BACKUPS_DIRECTORY/\$(date +%F_%Hh%Mm%Ss).tar.gz escape-server;
 "
 
 echo "2/3. Delete current 'escape-server' directory on raspberry pi..."
-sshpass -p 'raspberry' ssh pi@raspberrypi.local "
+sshpass -p $RASPBERRYPI_PASSWORD ssh pi@raspberrypi.local "
 sudo rm -rf escape-server;"
 
-current_git_directory=`git rev-parse --show-toplevel`
+CURRENT_GIT_DIRECTORY=`git rev-parse --show-toplevel`
 
 echo "3/3. Copying current git respository to rasperry pi..."
-sshpass -p 'raspberry' scp -r $current_git_directory pi@raspberrypi.local:/home/pi
+sshpass -p $RASPBERRYPI_PASSWORD scp -r $CURRENT_GIT_DIRECTORY pi@raspberrypi.local:/home/pi
 
 echo "DONE"
