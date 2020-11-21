be56072f348db76ef22a1d0b87db7f75d6f7f6af
Come Turrata <come@getluko.com>
2020-11-21 16:08:16 +0100

    Improve comment

--- scripts/deploy-to-raspberry-pi.sh
+++ scripts/deploy-to-raspberry-pi.sh
@@ -2,19 +2,19 @@
 
 backups_directory="escape-server-backups"
 
-echo "1. Backing up current 'escape-server' directory on rasperry pi..."
+echo "1/3. Backing up current 'escape-server' directory on rasperry pi..."
 sshpass -p 'raspberry' ssh pi@raspberrypi.local "
 mkdir -p $backups_directory;
 tar -czf $backups_directory/\$(date +%F_%H-%M-%S).tar.gz escape-server;
 "
 
-echo "2. Delete current 'escape-server' directory on raspberry pi..."
+echo "2/3. Delete current 'escape-server' directory on raspberry pi..."
 sshpass -p 'raspberry' ssh pi@raspberrypi.local "
 sudo rm -rf escape-server;"
 
 current_git_directory=`git rev-parse --show-toplevel`
 
-echo "3. Copying current git respository to rasperry pi..."
+echo "3/3. Copying current git respository to rasperry pi..."
 sshpass -p 'raspberry' scp -r $current_git_directory pi@raspberrypi.local:/home/pi
 
 echo "DONE"
